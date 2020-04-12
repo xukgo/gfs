d9c2e59241773dba7e892a0f4ddc1315738f9a2d
xukuan <xukuan@wxchina.com>
2020-04-12 23:29:08 +0800

    fix 1

---
+++ core/config.go
@@ -0,0 +1,90 @@
+package core
+
+import (
+	"flag"
+	"fmt"
+	log "github.com/sjqzhang/seelog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func init() {
+	flag.Parse()
+	if *v {
+		fmt.Printf("%s\n%s\n%s\n%s\n", VERSION, BUILD_TIME, GO_VERSION, GIT_VERSION)
+		os.Exit(0)
+	}
+	appDir, e1 := filepath.Abs(filepath.Dir(os.Args[0]))
+	curDir, e2 := filepath.Abs(".")
+	if e1 == nil && e2 == nil && appDir != curDir && !strings.Contains(appDir, "go-build") {
+		msg := fmt.Sprintf("please change directory to '%s' start fileserver\n", appDir)
+		msg = msg + fmt.Sprintf("请切换到 '%s' 目录启动 fileserver ", appDir)
+		log.Warn(msg)
+		fmt.Println(msg)
+		os.Exit(1)
+	}
+	DOCKER_DIR = os.Getenv("GFS_DIR")
+	if DOCKER_DIR != "" {
+		if !strings.HasSuffix(DOCKER_DIR, "/") {
+			DOCKER_DIR = DOCKER_DIR + "/"
+		}
+	}
+	STORE_DIR = DOCKER_DIR + STORE_DIR_NAME
+	CONF_DIR = DOCKER_DIR + CONF_DIR_NAME
+	DATA_DIR = DOCKER_DIR + DATA_DIR_NAME
+	LOG_DIR = DOCKER_DIR + LOG_DIR_NAME
+	STATIC_DIR = DOCKER_DIR + STATIC_DIR_NAME
+	LARGE_DIR_NAME = "haystack"
+	LARGE_DIR = STORE_DIR + "/haystack"
+	CONST_LEVELDB_FILE_NAME = DATA_DIR + "/fileserver.db"
+	CONST_LOG_LEVELDB_FILE_NAME = DATA_DIR + "/log.db"
+	CONST_STAT_FILE_NAME = DATA_DIR + "/stat.json"
+	CONST_CONF_FILE_NAME = CONF_DIR + "/cfg.json"
+	CONST_SERVER_CRT_FILE_NAME = CONF_DIR + "/server.crt"
+	CONST_SERVER_KEY_FILE_NAME = CONF_DIR + "/server.key"
+	CONST_SEARCH_FILE_NAME = DATA_DIR + "/search.txt"
+	FOLDERS = []string{DATA_DIR, STORE_DIR, CONF_DIR, STATIC_DIR}
+	logAccessConfigStr = strings.Replace(LOG_ACCESS_CONF_TEMPLATE, "{DOCKER_DIR}", DOCKER_DIR, -1)
+	logConfigStr = strings.Replace(LOG_CONF_TEMPLATE, "{DOCKER_DIR}", DOCKER_DIR, -1)
+	for _, folder := range FOLDERS {
+		os.MkdirAll(folder, 0775)
+	}
+	Singleton = NewServer()
+
+	peerId := fmt.Sprintf("%d", Singleton.util.RandInt(0, 9))
+	if !Singleton.util.FileExists(CONST_CONF_FILE_NAME) {
+		var ip string
+		if ip = os.Getenv("GFS_IP"); ip == "" {
+			ip = Singleton.util.GetPulicIP()
+		}
+		peer := "http://" + ip + ":8080"
+		cfg := fmt.Sprintf(CONF_JSON_TEMPLATE, peerId, peer, peer)
+		Singleton.util.WriteFile(CONST_CONF_FILE_NAME, cfg)
+	}
+	if logger, err := log.LoggerFromConfigAsBytes([]byte(logConfigStr)); err != nil {
+		panic(err)
+	} else {
+		log.ReplaceLogger(logger)
+	}
+	if _logacc, err := log.LoggerFromConfigAsBytes([]byte(logAccessConfigStr)); err == nil {
+		logacc = _logacc
+		log.Info("succes init log access")
+	} else {
+		log.Error(err.Error())
+	}
+	ParseConfig(CONST_CONF_FILE_NAME)
+	if Config().QueueSize == 0 {
+		Config().QueueSize = CONST_QUEUE_SIZE
+	}
+	if Config().PeerId == "" {
+		Config().PeerId = peerId
+	}
+	if Config().SupportGroupManage {
+		staticHandler = http.StripPrefix("/"+Config().Group+"/", http.FileServer(http.Dir(STORE_DIR)))
+	} else {
+		staticHandler = http.StripPrefix("/", http.FileServer(http.Dir(STORE_DIR)))
+	}
+	Singleton.initComponent(false)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/xukgo/gfs/core"
+
+func main() {
+	core.Singleton.Start()
+}
